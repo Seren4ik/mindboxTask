9655c7648e84ac29810a247ac6ce18f5b82c5b01
ss_kochetov <seren4ik9@mail.ru>
2023-01-28 15:17:29 +0300

    Addsql

---
+++ SqlQuery_1.sql
@@ -0,0 +1,4 @@
+﻿SELECT P.Name as 'Product', C.Name as 'Category'
+FROM Products P
+LEFT JOIN Products_To_Categories (NOLOCK) PTC ON PTC.ProductId = P.Id
+LEFT JOIN Categories (NOLOCK) C ON PTC.CategoryId = C.Id
